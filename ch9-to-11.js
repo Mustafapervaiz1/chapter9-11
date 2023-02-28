ecf0248709efd701956bcb8816091cc4748cf4cc
mustafapervez <syalani.training1@gmail.com>
2023-02-28 22:46:12 +0500

    question 6 complete

--- ch9-to-11.js
+++ ch9-to-11.js
@@ -1,10 +1,10 @@
-                                    //step 1 question no:1
+//step 1 question no:1
 /*var city = prompt("where you want to go")
 if(city=="karachi"){
     document.write("Welcome to the city of lights")
 }else {alert("your not allowed to come here")}*/
 
-                                    //step 2 question no:2
+//step 2 question no:2
 /*var gender = prompt("Gender Male or Female")
 if (gender =="male") {
   document.write("Good Morning Sir")
@@ -12,7 +12,7 @@ if (gender =="male") {
 if (gender == "female") {
   document.write("Good Morning Ma'am")
 }*/
-                                     //step 3 question no:3
+//step 3 question no:3
 /*var trafic_color = prompt(
   " what is traffic signal your (Red / Yellow / Green)"
 );
@@ -22,7 +22,90 @@ if ((trafic_color == "Red")) {
 if ((trafic_color == "Yellow")) {
   document.write("Ready to move");
 }
-if ((trafic_color = "Green")) {
+if ((trafic_color == "Green")) {
   document.write("Move now");
 } */
-                                  //step 4  question no:4
+//step 4  question no:4
+
+/*             var fuel = prompt("how much  liter fuel remain in your car fuel tank")
+if (fuel=="0.25 liters"){
+    document.write("Please refill the fuel in your car")
+}else{alert("okay go for your office")}*/
+
+                       //step 5  question no:5.a
+/*  var a = 4
+ if (++a===5){
+    alert(`given condition for variable ${a}  is true`)}
+   //  question no:5.b
+           var b = 82
+          if (b++===83){
+           alert(`given condition for variable ${b}  is true`)}
+           else{alert("this  83 is not true")}
+                           //question no:5.c
+          var c = 12
+          if (c++===13){
+            alert(`condition 1 is true`)}
+                if(c === 13){
+ alert(`condition 2 is true`)
+                }
+                if(++c < 14){  alert(`condition 3 is true`)}
+                if(c === 14){
+ alert(`condition 4 is true`)}*/
+
+                             //question no:5.d
+/*var materialCost = 20000
+var laborCost = 2000
+var totalCost = materialCost + laborCost
+if (totalCost == materialCost+ laborCost) {
+  alert("The total cost are equals")
+} else {
+  alert(" Total cost are not equal")
+}*/
+                          //question no:5.e
+ /*if (true) {
+   alert("true");
+ }
+ if (false) {
+   alert("false");
+ }
+                          //question no:5.f
+
+if ("car" > "cat"){
+  alert("car is smaller than cat")
+}*/
+                           //question no:6'
+
+/*var subject_a = (parseInt(prompt("write subject a marks ")))
+var subject_b = (parseInt(prompt("write subject b marks ")))
+var subject_c = (parseInt(prompt("write subject c marks ")))
+var total_marks = prompt("write total marks here")
+
+var obtained = (subject_a + subject_b + subject_c)
+var percentage = ((subject_a + subject_b + subject_c)/total_marks)*100
+percentage = Math.round(percentage)
+var grade,remarks 
+
+if(percentage >= 80){
+  grade="A-one"
+  remarks="Excellent"
+}else if ( percentage>= 70){
+  grade="A"
+  remarks="GOOD"
+}else if ( percentage>= 60){
+  grade="B"
+  remarks="You need to improve"
+}else{
+  grade="fail"
+  remarks="sorry"
+}
+
+
+document.write(`MARK SHEET </br></br></br>`)
+document.write(`Total marks:${total_marks}</br>`)
+document.write(`obtained marks:${obtained}</br>`)
+
+document.write(`percentage:${percentage}%</br>`)
+document.write(`Grade:${grade}</br>`)
+document.write(`Remarks:${remarks}</br>`)
+*/
+
