0fd19800fede0df3c124bd5addb5f83181395d4f
mustafapervez <syalani.training1@gmail.com>
2023-02-28 15:39:41 +0500

    2 step add

--- ch9-to-11.js
+++ ch9-to-11.js
@@ -1,1 +1,15 @@
-alert ("hello world")
+//step 1 question no:1
+/*var city = prompt("where you want to go")
+if(city=="karachi"){
+    document.write("Welcome to the city of lights")
+}else {alert("your not allowed to come here")}*/
+
+//step 2 question no:2
+/*var gender = prompt("Gender Male or Female")
+if (gender =="male") {
+  document.write("Good Morning Sir")
+}
+if (gender == "female") {
+  document.write("Good Morning Ma'am")
+}*/
+       //step 3 question no:3
