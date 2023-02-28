1b69c65266c84f4c0bc84d5df6c400cd20642f99
mustafapervez <syalani.training1@gmail.com>
2023-02-28 15:59:09 +0500

    step 3 done

--- ch9-to-11.js
+++ ch9-to-11.js
@@ -1,10 +1,10 @@
-//step 1 question no:1
+                                    //step 1 question no:1
 /*var city = prompt("where you want to go")
 if(city=="karachi"){
     document.write("Welcome to the city of lights")
 }else {alert("your not allowed to come here")}*/
 
-//step 2 question no:2
+                                    //step 2 question no:2
 /*var gender = prompt("Gender Male or Female")
 if (gender =="male") {
   document.write("Good Morning Sir")
@@ -12,4 +12,17 @@ if (gender =="male") {
 if (gender == "female") {
   document.write("Good Morning Ma'am")
 }*/
-       //step 3 question no:3
+                                     //step 3 question no:3
+/*var trafic_color = prompt(
+  " what is traffic signal your (Red / Yellow / Green)"
+);
+if ((trafic_color == "Red")) {
+  document.write("Must Stop");
+}
+if ((trafic_color == "Yellow")) {
+  document.write("Ready to move");
+}
+if ((trafic_color = "Green")) {
+  document.write("Move now");
+} */
+                                  //step 4  question no:4
