495c1c740f61908670b5db1b13a3121319f98221
mustafapervez <syalani.training1@gmail.com>
2023-03-01 01:27:10 +0500

    ch 10 without error

---
+++ ch9-to-11.js
@@ -0,0 +1,161 @@
+//step 1 question no:1
+/*var city = prompt("where you want to go")
+if(city=="karachi"){
+    document.write("Welcome to the city of lights")
+}else {alert("your not allowed to come here")}*/
+
+//step 2 question no:2
+/*var gender = prompt("Gender Male or Female")
+if (gender =="male") {
+  document.write("Good Morning Sir")
+}
+if (gender == "female") {
+  document.write("Good Morning Ma'am")
+}*/
+//step 3 question no:3
+/*var trafic_color = prompt(
+  " what is traffic signal your (Red / Yellow / Green)"
+);
+if ((trafic_color == "Red")) {
+  document.write("Must Stop");
+}
+if ((trafic_color == "Yellow")) {
+  document.write("Ready to move");
+}
+if ((trafic_color == "Green")) {
+  document.write("Move now");
+} */
+//step 4  question no:4
+
+/*             var fuel = prompt("how much  liter fuel remain in your car fuel tank")
+if (fuel=="0.25 liters"){
+    document.write("Please refill the fuel in your car")
+}else{alert("okay go for your office")}*/
+
+                       //step 5  question no:5.a
+/*  var a = 4
+ if (++a===5){
+    alert(`given condition for variable ${a}  is true`)}
+   //  question no:5.b
+           var b = 82
+          if (b++===83){
+           alert(`given condition for variable ${b}  is true`)}
+           else{alert("this  83 is not true")}
+                           //question no:5.c
+          var c = 12
+          if (c++===13){
+            alert(`condition 1 is true`)}
+                if(c === 13){
+ alert(`condition 2 is true`)
+                }
+                if(++c < 14){  alert(`condition 3 is true`)}
+                if(c === 14){
+ alert(`condition 4 is true`)}*/
+
+                             //question no:5.d
+/*var materialCost = 20000
+var laborCost = 2000
+var totalCost = materialCost + laborCost
+if (totalCost == materialCost+ laborCost) {
+  alert("The total cost are equals")
+} else {
+  alert(" Total cost are not equal")
+}*/
+                          //question no:5.e
+ /*if (true) {
+   alert("true");
+ }
+ if (false) {
+   alert("false");
+ }
+                          //question no:5.f
+
+if ("car" > "cat"){
+  alert("car is smaller than cat")
+}*/
+                           //question no:6'
+
+/*var subject_a = (parseInt(prompt("write subject a marks ")))
+var subject_b = (parseInt(prompt("write subject b marks ")))
+var subject_c = (parseInt(prompt("write subject c marks ")))
+var total_marks = prompt("write total marks here")
+
+var obtained = (subject_a + subject_b + subject_c)
+var percentage = ((subject_a + subject_b + subject_c)/total_marks)*100
+percentage = Math.round(percentage)
+var grade,remarks 
+
+if(percentage >= 80){
+  grade="A-one"
+  remarks="Excellent"
+}else if ( percentage>= 70){
+  grade="A"
+  remarks="GOOD"
+}else if ( percentage>= 60){
+  grade="B"
+  remarks="You need to improve"
+}else{
+  grade="fail"
+  remarks="sorry"
+}
+`                                  
+
+document.write(`MARK SHEET </br></br></br>`)
+document.write(`Total marks:${total_marks}</br>`)
+document.write(`obtained marks:${obtained}</br>`)
+
+document.write(`percentage:${percentage}%</br>`)
+document.write(`Grade:${grade}</br>`)
+document.write(`Remarks:${remarks}</br>`)
+*/
+                                 //question no:7'
+
+
+/*var input_number = prompt("guess one number ranging 1 to 10")
+var secret_number = 5;
+
+if(input_number == secret_number){
+  alert("Bingo!")
+}else if (input_number == ++secret_number){
+  document.write("Close enough to the close answer")
+}else {alert("sorry try again later")}*/
+
+
+                                    //question no:8' uncompletexxxxxxxxxxxxxxxxxxxxxx
+
+                                    //question no:9' 
+
+/*var num = prompt("write a number check even or odd number")
+
+if( num%2===0)
+{alert("this is even number")}else {alert("this is odd number")}*/
+
+                                    //question no:10'
+
+/*var temperature =prompt("todya weather 40 / 30 / 20 10")
+
+if(temperature >= 40){
+  alert("It is to hot  outside")} 
+
+  if (temperature >= 30){
+   alert("The weather today is normal")
+ }
+  if(temperature >= 20){
+   alert("Today weather  is cool")
+ }
+   if(temperature >= 10){
+   alert("OMG!Today weather is so cool")
+ }*/
+                                   //question no:11
+
+
+    var first_number = prompt("write your first value")                               
+    var second_number = prompt("write you  second value")                               
+    var operator = prompt("define operator plz (+ - * /)")
+    var result =(`${first_number}${operator}${second_number}`)
+    alert(result)
+    if (operator=="+"){
+      alert(result)
+    }
+     
+    
